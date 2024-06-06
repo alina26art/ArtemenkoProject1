bb4dbff9723fc71beac1243580f58ca2f4e124b2
alina26art <165063131+alina26art@users.noreply.github.com>
2024-06-06 22:52:48 +0300

    update java

--- script.js
+++ script.js
@@ -19,7 +19,36 @@ function myFunction() {
 function checkOperation(id, operator) {
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
-    const sum = parseFloat(document.getElementById('sum').value);
+    const result = parseFloat(document.getElementById(id).value);
     const resultElement = document.getElementById('result');
 
-    if (isNaN(num1) || isNaN(num2)
+    if (isNaN(num1) || isNaN(num2) || isNaN(result)) {
+        resultElement.textContent = "Please enter valid numbers.";
+        return;
+    }
+
+    let operationResult;
+    switch (operator) {
+        case '+':
+            operationResult = num1 + num2;
+            break;
+        case '-':
+            operationResult = num1 - num2;
+            break;
+        case '*':
+            operationResult = num1 * num2;
+            break;
+        case '/':
+            operationResult = num1 / num2;
+            break;
+        default:
+            resultElement.textContent = "Invalid operator";
+            return;
+    }
+
+    if (operationResult === result) {
+        resultElement.textContent = "The operation is correct!";
+    } else {
+        resultElement.textContent = "The operation is incorrect!";
+    }
+}
